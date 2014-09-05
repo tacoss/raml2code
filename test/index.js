bbe06f2d9ec90ee24a11aa96d721f443e77b2bdf
atomsfat <atomsmail@gmail.com>
2014-09-05 15:01:30 -0500

    updating index

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var Mocha = require('mocha');
 var chai = require('chai');
 
 var mocha = new Mocha();
-mocha.addFile(__dirname + '/raml2Code-spec');
+mocha.addFile(__dirname + '/data2Code-spec');
 
 chai.should();
 mocha.run();
