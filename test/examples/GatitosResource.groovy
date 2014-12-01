90cf4b4b5081832006911dbf4baba56af299de8c
atomsfat <atomsmail@gmail.com>
2014-12-01 13:01:44 -0600

    fixing mess with imports thank to @iamedu

--- test/examples/GatitosResource.groovy
+++ test/examples/GatitosResource.groovy
@@ -4,7 +4,6 @@ import javax.ws.rs.core.*
 import org.gex.dto.v1.*
 
 import org.glassfish.jersey.media.multipart.*
-import retrofit.mime.TypedFile
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON
 
 @Path("/cats")
