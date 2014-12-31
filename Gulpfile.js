2446dc806e899c94b035a9e2076c9e9b96152959
atomsfat <atomsmail@gmail.com>
2014-12-31 15:32:36 -0600

    solving issue with typo on cats.raml

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,30 @@
 var gulp = require('gulp');
+var del = require('del');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var coffee = require('gulp-coffee');
 
-gulp.task('coffee', ['copy-templates'], function() {
-  gulp.src('./src/**/*.coffee')
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./lib/'))
+gulp.task('clean', function (cb) {
+  del([
+    'lib/**'
+  ], cb);
 });
 
-gulp.task('copy-templates', function(){
+
+gulp.task('coffee', ['clean'], function(cb) {
+  gulp.src('./src/**/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./lib/'));
+  cb(null);
+});
+
+gulp.task('copy-templates', ['clean'], function(cb){
   var stream = gulp.src('./src/**/*.hbs')
   .pipe(gulp.dest('./lib/'));
   return stream;
 });
 
-gulp.task('test', ['coffee', 'copy-templates'], function(cb){
+gulp.task('test', ['copy-templates', 'coffee'], function(cb){
   gulp.src(['./test/**/*spec.js'])
   .pipe(mocha(
     {
