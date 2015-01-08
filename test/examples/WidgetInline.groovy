da0979e2a0022a4f9a6fb9c6e90de29cc1468a05
atomsfat <atomsmail@gmail.com>
2015-01-08 10:30:05 -0600

    updating dependency test

--- test/examples/WidgetInline.groovy
+++ test/examples/WidgetInline.groovy
@@ -12,35 +12,9 @@ public class Widget implements Serializable {
   @NotNull
   String name
 
-  
-  Composite composite
-
 
   List<String> etiquetas
 
 }
 
-class Composite implements Serializable {
 
-  /* The unique identifier of the widget  */
-  String xid
-
-  /* The friendly name of the widget  */
-  String name
-
-  /* Common set of keywords to categorize widgets */
-  List tags
-
-  /* The version of the Widget  */
-  String groupId
-
-  /* The version of the Widget  */
-  String artifactId
-
-  /* The version of the Widget  */
-  String revision
-
-  /* The description of the Widget  */
-  String description
-
-}
